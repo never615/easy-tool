464e1712b746c61d2141249ce8f8dc481f0d99bc
never615 <583531034@qq.com>
2017-05-08 14:40:20 +0800

    init

---
+++ src/Lottery/Lottery.php
@@ -0,0 +1,11 @@
+<?php
+namespace Mallto\Tool\Lottery;
+/**
+ * Created by PhpStorm.
+ * User: never615
+ * Date: 06/05/2017
+ * Time: 4:19 PM
+ */
+class Lottery{
+    
+}
---
+++ src/SensitiveWord/SensitiveWord.php
@@ -0,0 +1,13 @@
+<?php
+namespace Mallto\Tool\SensitiveWord;
+
+/**
+ * Created by PhpStorm.
+ * User: never615
+ * Date: 06/05/2017
+ * Time: 4:19 PM
+ */
+class SensitiveWord
+{
+    
+}
