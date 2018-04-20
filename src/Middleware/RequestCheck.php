eb6ff6e69ed34a92a8870a5d28d4b199bab4ee95
never615 <583531034@qq.com>
2018-04-20 15:33:48 +0800

    requestCheck中间件设置header mode 为api

--- src/Middleware/RequestCheck.php
+++ src/Middleware/RequestCheck.php
@@ -7,10 +7,18 @@ namespace Mallto\Tool\Middleware;
 
 
 use Closure;
+use Illuminate\Http\Request;
 use Mallto\Admin\SubjectUtils;
 use Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException;
 use Symfony\Component\HttpKernel\Exception\PreconditionRequiredHttpException;
 
+/**
+ * api 接口请求专用
+ *
+ * Class RequestCheck
+ *
+ * @package Mallto\Tool\Middleware
+ */
 class RequestCheck
 {
 
@@ -19,7 +27,7 @@ class RequestCheck
      * @param Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
 
         $uuid = SubjectUtils::getUUID();
@@ -32,6 +40,8 @@ class RequestCheck
         if (!in_array($requestType, config('mall.request_type'))) {
             throw new PreconditionFailedHttpException(trans("errors.precondition_failed"));
         }
+
+        $request->headers->set("mode", "api");
 
         return $next($request);
     }
