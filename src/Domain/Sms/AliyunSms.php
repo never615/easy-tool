2021f7f8cda11abe3748ad0e380f93bafccd0465
never615 <583531034@qq.com>
2018-09-03 11:09:21 +0800

    阿里云短信发送异常处理优化

--- src/Domain/Sms/AliyunSms.php
+++ src/Domain/Sms/AliyunSms.php
@@ -207,10 +207,17 @@ class AliyunSms extends AbstractAPI implements Sms
     protected function checkAndThrow(
         array $contents
     ) {
-//        \Log::debug($contents);
-        if ($contents['Code'] != 'OK') {
-            throw new ResourceException("短信发送失败:".$contents['Code'].",".$contents["Message"]);
+        switch ($contents['Code']) {
+            case "isv.BUSINESS_LIMIT_CONTROL":
+                throw new ResourceException("一分钟内只能发送一条短信");
+                break;
+            default:
+                \Log::error("短信发送失败,checkAndThrow");
+                \Log::warning($contents);
+                throw new ResourceException("短信发送失败:".$contents['Code'].",".$contents["Message"]);
+                break;
         }
+
     }
 
 
