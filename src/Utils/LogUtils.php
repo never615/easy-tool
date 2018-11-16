24de424ac3f058ff7b4029ef36bc8fe7070ad618
never615 <583531034@qq.com>
2018-11-16 14:43:17 +0800

    add logUtils

---
+++ src/Utils/LogUtils.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright (c) 2018. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+namespace Mallto\Tool\Utils;
+
+/**
+ * Created by PhpStorm.
+ * User: never615 <never615.com>
+ * Date: 2018/11/16
+ * Time: 2:32 PM
+ */
+class LogUtils
+{
+    /**
+     * 系统参数未配置
+     *
+     * 需要由项目拥有者进行配置
+     *
+     * @param      $msg
+     */
+    public static function notConfigLogByOwner($msg)
+    {
+        \Log::warning("owner_config:".$msg);
+    }
+
+
+    /**
+     * 系统参数未配置
+     *
+     * 需要由subject管理者配置
+     *
+     * @param $msg
+     */
+    public static function notConfigLogBySubjecter($msg)
+    {
+        \Log::warning("subjecter_config:".$msg);
+    }
+
+}
