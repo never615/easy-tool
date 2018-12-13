3566855edb526c3070a9e2d7a1b460a9951967db
never615 <583531034@qq.com>
2018-12-13 12:21:33 +0800

    no message

---
+++ migrations/2018_12_13_132725_update_ads_add_switch.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (c) 2018. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * 广告管理添加开关
+ * Class UpdateAdsAddSwitch
+ */
+class UpdateAdsAddSwitch extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            $table->boolean("switch")->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('page_pv_manager', function (Blueprint $table) {
+            $table->dropColumn("switch");
+        });
+    }
+}
