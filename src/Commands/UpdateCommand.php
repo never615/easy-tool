ed509c28aa8f4b494758834553a5a937fe394508
never615 <583531034@qq.com>
2018-12-17 16:42:27 +0800

    菜单更新

--- src/Commands/UpdateCommand.php
+++ src/Commands/UpdateCommand.php
@@ -6,7 +6,6 @@
 namespace Mallto\Tool\Commands;
 
 use Illuminate\Console\Command;
-use Mallto\Tool\Seeder\PermissionTablesSeeder;
 use Mallto\Tool\Seeder\TablesSeeder;
 
 class UpdateCommand extends Command
@@ -39,7 +38,8 @@ class UpdateCommand extends Command
      */
     public function handle()
     {
-        $this->call('db:seed', ['--class' => PermissionTablesSeeder::class]);
+//        $this->call('db:seed', ['--class' => PermissionTablesSeeder::class]);
+        $this->call('db:seed', ['--class' => TablesSeeder::class]);
 
     }
 
