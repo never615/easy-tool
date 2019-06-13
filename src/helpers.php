c6ac83eb06460479a96c288d81ad6ac7f8347a5a
never615 <583531034@qq.com>
2019-06-13 19:48:27 +0800

    no message

---
+++ src/helpers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2018. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+if (!function_exists('json_decode1')) {
+
+
+    function json_decode1($json, $assoc = false, $depth = 512, $options = 0)
+    {
+        if(is_array($json)){
+            return $json;
+        }
+        return json_decode($json, $assoc, $depth, $options);
+    }
+}
+
+
+
