e2ccbde0c8faab24334e41f18125e67a0798ce64
never615 <never615@gmail.com>
2019-09-26 17:14:01 +0800

    中间件统一检查资源路由上{id}参数是否合规

--- src/Middleware/RequestCheck.php
+++ src/Middleware/RequestCheck.php
@@ -8,10 +8,10 @@ namespace Mallto\Tool\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Mallto\Admin\SubjectUtils;
 use Mallto\Tool\Exception\ResourceException;
-use Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException;
 use Symfony\Component\HttpKernel\Exception\PreconditionRequiredHttpException;
 
 /**
@@ -38,12 +38,25 @@ class RequestCheck
      */
     public function handle(Request $request, Closure $next)
     {
+        $route = $request->route();
+
+        //检查资源路由器上的id,
+        //如:Route::resource("shop", 'ShopController'); ,请求shop/111时,如果111是非数字字符,则会走此校验
+        $actionMethod = $route->getActionMethod();
+        if ($actionMethod == "show") {
+            $firstRouterParam = Arr::first(($route->parameters()));
+            if ($firstRouterParam) {
+                if (!preg_match('/\d/', $firstRouterParam)) {
+                    throw new ResourceException("无效的查询参数:".$firstRouterParam);
+                }
+            }
+        }
+
         $uuid = SubjectUtils::getUUID();
         if (!$uuid) {
             throw new PreconditionRequiredHttpException(trans("errors.precondition_request"));
         }
 
-        $requestType = $request->header('REQUEST_TYPE');
 
         $user = Auth::guard("api")->user();
         //如果user存在,检查user和uuid是否一致
@@ -59,10 +72,11 @@ class RequestCheck
             }
         }
 
-
-        if ($requestType && !in_array($requestType, $this->requestTypes)) {
-            throw new PreconditionFailedHttpException(trans("errors.precondition_failed"));
-        }
+//        $requestType = $request->header('REQUEST_TYPE');
+//
+//        if ($requestType && !in_array($requestType, $this->requestTypes)) {
+//            throw new PreconditionFailedHttpException(trans("errors.precondition_failed"));
+//        }
 
         $request->headers->set("mode", "api");
 
