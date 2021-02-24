6a556688dac6263b7dcfb9f9986d0026228d6755
never615 <never615@gmail.com>
2021-02-24 20:14:34 +0800

    app secret 的slug自动设置保存

--- src/Controller/Admin/AppSecretRoleController.php
+++ src/Controller/Admin/AppSecretRoleController.php
@@ -10,9 +10,12 @@ use Encore\Admin\Grid;
 use Mallto\Admin\Controllers\Base\AdminCommonController;
 use Mallto\Tool\Data\AppSecretsPermission;
 use Mallto\Tool\Data\AppSecretsRole;
+use Mallto\Tool\Domain\Traits\SlugAutoSave;
 
 class AppSecretRoleController extends AdminCommonController
 {
+
+    use SlugAutoSave;
 
     /**
      * 获取这个模块的标题
@@ -39,7 +42,7 @@ class AppSecretRoleController extends AdminCommonController
     protected function gridOption(Grid $grid)
     {
         $grid->column('name', '角色名称')->editable();
-        $grid->column('slug', '角色标识')->editable();
+        //$grid->column('slug', '角色标识')->editable();
 
         $grid->filter(function (Grid\Filter $filter) {
             $filter->ilike('name', '角色名称');
@@ -52,14 +55,21 @@ class AppSecretRoleController extends AdminCommonController
 
     protected function formOption(Form $form)
     {
-        $form->text('name');
-        $form->textarea('slug');
+        $form->text('name')
+            ->help('不能重复');
+        //$form->textarea('slug');
         $form->checkbox('permissions', trans('admin.permissions'))
             ->options(function () {
                 return AppSecretsPermission::query()->orderBy('created_at')->get()->pluck('name',
                     'id')->toArray();
             })
             ->stacked();
+
+        $form->saving(function (Form $form) {
+            $this->slugSavingCheck($form);
+        });
+
+
     }
 
 }
