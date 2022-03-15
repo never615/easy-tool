70470940b8469d0febb21f504a05b0d90dec1826
never615 <never615@gmail.com>
2022-03-15 17:31:57 +0800

    add cacheutils

---
+++ src/Utils/CacheUtils.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Copyright (c) 2022. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+namespace Mallto\Tool\Utils;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+
+/**
+ * User: never615 <never615.com>
+ * Date: 2022/3/15
+ * Time: 5:04 下午
+ */
+class CacheUtils
+{
+
+    public static function clear($prefix)
+    {
+        $cachePrefix = config('app.unique') . '_' . config('app.env')
+            . ':' . $prefix . '*';
+
+        $keys = Redis::connection('cache')
+            ->keys($cachePrefix);
+
+        Redis::connection('cache')->del($keys);
+    }
+}
