298c49ce02130fdf60e22cbc564e367488056557
never615 <never615@gmail.com>
2024-05-09 17:06:45 +0800

    select数据支持tag

--- src/Controller/Admin/SelectSource/SelectSourceExtend.php
+++ src/Controller/Admin/SelectSource/SelectSourceExtend.php
@@ -5,9 +5,11 @@
 
 namespace Mallto\Tool\Controller\Admin\SelectSource;
 
+use Illuminate\Support\Facades\DB;
 use Mallto\Admin\Controllers\Base\SelectSourceExtendInterface;
 use Mallto\Tool\Data\Ad;
 use Mallto\Tool\Data\PagePvManager;
+use Mallto\Tool\Data\Tag;
 
 /**
  * User: never615 <never615.com>
@@ -39,14 +41,14 @@ class SelectSourceExtend implements SelectSourceExtendInterface
             if ($pagePvManager) {
                 $adTypes = $pagePvManager->ad_types;
 
-                $adTypes = array_unique(array_merge($adTypes, [ "float_image" ]));
+                $adTypes = array_unique(array_merge($adTypes, ["float_image"]));
 
                 $adTypes = array_only(Ad::AD_TYPE, $adTypes);
 
                 $temps = [];
                 foreach ($adTypes as $adTypesKey => $value) {
                     $temps[] = [
-                        "id"   => $adTypesKey,
+                        "id" => $adTypesKey,
                         "text" => $value,
                     ];
                 }
@@ -57,6 +59,40 @@ class SelectSourceExtend implements SelectSourceExtendInterface
             }
 
         }
+
+
+        if ($key === 'tag') {
+            if (!is_null($id)) {
+                $id = explode(",", $id);
+
+                return Tag::query()
+                    ->select(DB::raw("id,name as text"))
+                    ->findOrFail($id);
+            } else {
+                if (!$adminUser->isOwner()) {
+                    $query = Tag::query()
+                        ->select(DB::raw("tags.id,tags.name||'-('||subjects.name||')' as text"))
+                        ->join('subjects', 'subjects.id', 'subject_id')
+                        ->whereIn("tags.subject_id", $childSubjectIds)
+                        ->orderBy('tags.created_at', 'desc');
+
+                    $query->where('tags.name', '~*', "$q");
+
+                    return $query->paginate($perPage, ['id', 'text']);
+                } else {
+                    $query = Tag::query()
+                        ->select(DB::raw('tags.id,tags.name as text'))
+                        ->join('subjects', 'subjects.id', 'subject_id')
+                        ->whereIn("tags.subject_id", $childSubjectIds)
+                        ->orderBy('tags.created_at', 'desc');
+
+                    $query->where('tags.name', '~*', "$q");
+
+                    return $query->paginate($perPage, ['id', 'text']);
+                }
+            }
+        }
+
     }
 
 
