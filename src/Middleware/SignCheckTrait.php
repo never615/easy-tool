ac65eb80d051da7a5b6700e6575fa28f93470837
never615 <never615@gmail.com>
2024-11-06 15:52:23 +0800

    fix bug: app-id 999 测试环境特殊处理.

--- src/Middleware/SignCheckTrait.php
+++ src/Middleware/SignCheckTrait.php
@@ -32,15 +32,15 @@ trait SignCheckTrait
 
     protected function check(Request $request, Closure $next)
     {
-        if ( ! config('other.auth_sign')) {
+        if (!config('other.auth_sign')) {
             return $next($request);
         }
 
         $appId = $request->header("app_id");
         $appSecret = AppSecret::where("app_id", $appId)->first();
 
-        if ( ! $appSecret || ! $appSecret->switch) {
-            Log::warning("app_id 无效:".$appId);
+        if (!$appSecret || !$appSecret->switch) {
+            Log::warning("app_id 无效:" . $appId);
             Log::warning($request->url());
             throw new SignException("app_id 无效");
         }
@@ -138,14 +138,14 @@ trait SignCheckTrait
                 $signature = $request->header("signature");
 
                 if (is_null($timestamp) || is_null($uuid) || is_null($appId) || is_null($signatureNonce) || is_null($signature)) {
-                    throw new PreconditionRequiredHttpException('sign auth fail:'.trans("errors.precondition_request"));
+                    throw new PreconditionRequiredHttpException('sign auth fail:' . trans("errors.precondition_request"));
                 }
 
                 //校验uuid是否属于开发者关联的subject或者他的子主体
                 $this->subjectCheck($appSecret);
 
                 //时间戳格式检查
-                if ( ! Carbon::hasFormat($timestamp, "Y-m-d H:i:s")) {
+                if (!Carbon::hasFormat($timestamp, "Y-m-d H:i:s")) {
                     throw new ResourceException("InvalidTimeStamp.Format");
                 }
 
@@ -153,20 +153,20 @@ trait SignCheckTrait
 
                 $requestPath = $request->path();
 
-                $nonce = $uuid.$appId.$requestPath.$signatureNonce;
+                $nonce = $uuid . $appId . $requestPath . $signatureNonce;
                 if (Cache::get($nonce)) {
-                    throw new ResourceException("请求已被接受,signature_nonce:".$signatureNonce);
+                    throw new ResourceException("请求已被接受,signature_nonce:" . $signatureNonce);
                 }
 
                 if (Carbon::now()->subMinutes(3) < $timestamp) {
                     //和当前时间间隔比较在15分钟内
                     //检查签名
                     if (SignUtils::verifySign4(array_merge($inputs, [
-                        "timestamp"       => $timestamp,
-                        $uuidKey          => $uuid,
-                        "app_id"          => $appId,
+                        "timestamp" => $timestamp,
+                        $uuidKey => $uuid,
+                        "app_id" => $appId,
                         "signature_nonce" => $signatureNonce,
-                        "signature"       => $signature,
+                        "signature" => $signature,
                     ]), $secret)) {
                         //pass
                         Cache::put($nonce, 1, 3 * 60);
@@ -188,6 +188,10 @@ trait SignCheckTrait
 
     public function subjectCheck($appSecret)
     {
+        if ($appSecret->app_id == '999' && AppUtils::isTestEnv()) {
+            return;
+        }
+
         //获取开发者关联的主体
         $subjects = $appSecret->app_secret_subjects->toarray();
         if (empty($subjects)) {
@@ -195,7 +199,7 @@ trait SignCheckTrait
         }
         //获取uuid对应的主体id
         $appSecretSubjectId = SubjectUtils::getSubjectId();
-        if ( ! in_array($appSecretSubjectId, array_column($subjects, 'id'), true)) {
+        if (!in_array($appSecretSubjectId, array_column($subjects, 'id'), true)) {
             throw new ResourceException("权限不足,请联系管理员");
         }
     }
@@ -212,7 +216,7 @@ trait SignCheckTrait
     public function permissionCheck(Request $request, $appSecretUser)
     {
         //没有开启的校验的直接跳过
-        if ( ! $appSecretUser->is_check_third_permission) {
+        if (!$appSecretUser->is_check_third_permission) {
             return true;
         }
 
@@ -222,8 +226,8 @@ trait SignCheckTrait
         if (count($routenameArr) == 2) {
             $subRouteName0 = $routenameArr[0];
 
-            if ( ! AppSecretsPermission::query()->where('slug', $routeName)->exists()) {
-                $routeName = $subRouteName0.'.*';
+            if (!AppSecretsPermission::query()->where('slug', $routeName)->exists()) {
+                $routeName = $subRouteName0 . '.*';
             }
 
         }
