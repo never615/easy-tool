1e8ef548542d6967ae4d18e88d76be313d58d346
never615 <never615@gmail.com>
2025-05-28 23:06:41 +0800

    自定义time 验证器,匹配HH:MM:SS格式，允许小时为00-99

--- src/Providers/ToolServiceProvider.php
+++ src/Providers/ToolServiceProvider.php
@@ -173,6 +173,10 @@ class ToolServiceProvider extends ServiceProvider
             return filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) !== null;
         });
 
+        Validator::extend('custom_time', function ($attribute, $value, $parameters, $validator) {
+            // 匹配HH:MM:SS格式，允许小时为00-99
+            return preg_match('/^(0\d|1\d|2\d|3\d|4\d|5\d|6\d|7\d|8\d|9\d):([0-5]\d):([0-5]\d)$/', $value);
+        });
         //自定义响应方法
         Response::macro('nocontent', function () {
             return Response::make('', 204);
